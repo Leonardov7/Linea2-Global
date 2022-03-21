df4aeaa148fd6e9efc893a49e5d5e1792aa5213b
leonardo <leonardov7@gmail.com>
2022-03-20 22:22:12 -0500

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'View/Login.dart';
+import 'View/UserRegister.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +29,48 @@ class HomeStart extends State<Home> {
         appBar: AppBar(
           title: Text('Home App línea 2'),
         ),
-
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('image/User.png'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(minimumSize: Size(400, 50)),
+                  onPressed: () {
+                    print('Ingreso Login');
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Login()));
+                  },
+                  child: Text('Ingresar'),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(minimumSize: Size(400, 50)),
+                  onPressed: () {
+                    print('Registrar Usuario');
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => UserRegister()));
+                  },
+                  child: Text('Registrar Usuario'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

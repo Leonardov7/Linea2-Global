b5118a100f8ff38e7f5639668b29d1febb469b6a
leonardo <leonardov7@gmail.com>
2022-03-21 08:45:13 -0500

    Data Picker

--- lib/View/Login.dart
+++ lib/View/Login.dart
@@ -6,30 +6,117 @@ class Login extends StatefulWidget{//cambia dinámicamente
   LoginApp createState()=> LoginApp();
 }
 
-class LoginApp extends State<Login>{
-  
+class LoginApp extends State<Login> {
+  TextEditingController user=TextEditingController();
+  TextEditingController pass=TextEditingController();
+  DateTime selectedDate = DateTime.now();
+  var _currentSelectedDate;
+
+
+  validarDatos()async{
+    try{
+      CollectionReference ref =FirebaseFirestore.instance.collection("User");
+      QuerySnapshot usuario=await ref.get();
+
+      if (usuario.docs.length !=0){
+
+        for(var cursor in usuario.docs){
+          if (user.text==cursor.get('User')){
+            if (pass.text==cursor.get('Pass')){
+              mensaje('Mensaje', 'dato encontrado');
+            }
+          }
+          //print(cursor.get('User'));
+        }
+      }
+    }catch(e){
+      mensaje('Error', e.toString());
+    }
+  }
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ingreso de Clientes'),  
+        title: Text('Login'),
+
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(padding: EdgeInsets.all(10),
-            child: Center(
-              child: Container(
-                width: 100,
-                height: 100,
-                child: Image.asset('image/User.png'),
+            Padding(
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset('image/User.png'),
+                  ),
+                )),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'User',
+                  hintText: 'Digite el usuario',
+                ),
               ),
             ),
-            )
-            
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: pass,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Pass',
+                  hintText: 'Digite la contraseña',
+
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(minimumSize: Size(500,50)),
+
+                onPressed: (){
+                   validarDatos();
+
+                  pass.clear();
+                  //mensaje('Este es un título', 'Este es un mensaje');
+                },
+                child: Text('Ingresar'),
+              ),
+            ),
+            Text("$_currentSelectedDate")
           ],
+
         ),
       ),
     );
+  }
+
+  void mensaje(String titulo, String mess) {
+    showDialog(
+        context: context,
+        builder: (buildcontext) {
+          return AlertDialog(
+            title: Text(titulo),
+            content: Text(mess),
+            actions: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child:
+                Text("Aceptar", style: TextStyle(color: Colors.blueGrey)),
+              )
+            ],
+          );
+        });
   }
 }
--- lib/View/UserRegister.dart
+++ lib/View/UserRegister.dart
@@ -1,19 +1,225 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UserRegister extends StatefulWidget {
   @override
   UserRegisterApp createState() => UserRegisterApp();
 }
-class UserRegisterApp extends State<UserRegister>{
+
+class UserRegisterApp extends State<UserRegister> {
+  DateTime selectedDate = DateTime.now();
+  TextEditingController fecha = TextEditingController();
+  var _currentSelectedDate;
+
+  void callDataPcker() async {
+    var selectedDate = await getDatePickerWidget();
+    setState(() {
+      _currentSelectedDate = selectedDate;
+      fecha.text=selectedDate.toString();
+    });
+  }
+
+  Future<DateTime?> getDatePickerWidget() {
+    return showDatePicker(
+        context: context,
+        initialDate: DateTime(2022),
+        firstDate: DateTime(1930),
+        lastDate: DateTime.now(),
+        fieldHintText: "DIA/MES/AÑO",
+        fieldLabelText: "FECHA DE NACIMIENTO",
+        helpText: "FECHA DE NACIMIENTO",
+        errorFormatText: "Ingrese una fecha válida",
+        errorInvalidText: "Fecha fuera de rango",
+        initialEntryMode: DatePickerEntryMode.input,
+        builder: (context, child) {
+          return Theme(data: ThemeData.fallback(), child: Center(child: child));
+        });
+  }
+
+  var flag = false;
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Registro de usuario'),
       ),
       body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(left: 5, top: 10)),
+            Container(
+              width: 500,
+              height: 70,
+              child: Image.asset('image/img.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Nombres',
+                  hintText: 'Digite su nombre y apellido',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Identificación',
+                  hintText: 'Digite número de identificación',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Correo',
+                  hintText: 'Digite su Correo',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Teléfono',
+                  hintText: 'Digite su teléfono',
+                ),
+              ),
+            ),
+          /*  Focus(
+                child: TextField(
+                  controller: fecha,
+                  style: TextStyle(color: Colors.blueGrey),
+                  decoration: InputDecoration(
+                    fillColor: Colors.green,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: 'Fecha',
+                    hintText: 'Digite la fecha de nacimiento',
+                    // print('2:  $hasFocus');
+                  ),
+                ),
+                onFocusChange: (hasFocus) {
+                  //    print('$hasFocus');
+                  if (hasFocus == true) {
+                    //  flag=true;
+                    print(hasFocus);
+                    //callDataPcker();
+                    print(_currentSelectedDate.toString());
+                    fecha.text = _currentSelectedDate.toString();
+                    //   hasFocus=false;
+                  }
+                  ;
+                }),
 
+           */
+            Padding(
+                padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+                child: Stack(
+                    alignment: const Alignment(1.0, 1.0),
+                    children: <Widget>[
+                      new TextField(
+                        enabled: false,
+                        controller: fecha,
+                        style: TextStyle(color: Colors.blueGrey),
+                        decoration: InputDecoration(
+                          fillColor: Colors.green,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          labelText: 'Fecha',
+                          hintText: 'Digite la fecha de nacimiento',
+                        ),
+                      ),
+                      new FlatButton(
+                          onPressed: () {
+                            callDataPcker();
+
+                          },
+                          child: new Icon(Icons.date_range))
+                    ])),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //onFocusChange: (hasFocus) {
+                //print('2:  $hasFocus');
+                //},
+                // onChanged: (Text) {
+                // callDataPcker();
+                //},
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Fecha de nacimiento',
+                  hintText: 'Digite su nombre',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Contraseña',
+                  hintText: 'Digite su contraseña',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+              child: TextField(
+                //controller: user,
+                style: TextStyle(color: Colors.blueGrey),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Confirmación de contraseña',
+                  hintText: 'Confirme la contraseña',
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(left: 15,top: 10,right: 15),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: Size(400, 50)),
+              onPressed: (){
+
+              },
+              child: Text('Enviar'),
+            ),
+            )
+          ],
+        ),
       ),
-    ) ;
+    );
   }
 }
